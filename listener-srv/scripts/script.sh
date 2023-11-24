881ef0a6995704ee6069e1c1eddb72425779f43d
ulyh <janeuly@pm.me>
2023-11-24 19:20:46 +0100

    modified scripts to work on listener server

--- listener-srv/scripts/script.sh
+++ listener-srv/scripts/script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Starting script..."
-docker pull ulyhmyges/client-ci
-docker run --rm -d --name client-ci-c --platform linux/amd64 ulyhmyges/client-ci
+docker compose down
+docker pull ulyhmyges/client-ci:v1.0.0
+docker pull ulyhmyges/b-test-ci:v1.0.0
+docker compose up -d --build
+
+#docker run --rm -d --name client-ci-c --platform linux/amd64 ulyhmyges/client-ci:v1.0.0
 echo "Script ended"
